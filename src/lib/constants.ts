c9a2093a9cdc53bbf4049b2659ba7f6ee2d68207
somaya-77 <alkhtybsmytadl@gmail.com>
2025-03-04 01:55:54 +0200

    00

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const ARTICLE_PER_PAGE = 6;
 
-const PRODUCTION_DOMAIN = "https://cloud-hosting-project-course.vercel.app";
+const PRODUCTION_DOMAIN = "https://snapgram-social-media-app.netlify.app";
 const DEVELOPMENT_DOMAIN = "http://localhost:3000";
 
 export const DOMAIN = process.env.NODE_ENV === 'production' 
