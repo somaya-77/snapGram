10a09370eb3ecab8943813838a4892d37b348fa9
somaya-77 <alkhtybsmytadl@gmail.com>
2025-03-04 22:55:11 +0200

    solve domain

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,9 +3,16 @@ export const ARTICLE_PER_PAGE = 6;
 const PRODUCTION_DOMAIN = "https://snapgram-social-media-app.netlify.app";
 const DEVELOPMENT_DOMAIN = "http://localhost:3000";
 
-export const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN || "http://localhost:3000";
+export const DOMAIN = "https://snapgram-social-media-app.netlify.app";
 
 
-    console.log("Current Environment:", process.env.NODE_ENV);
-console.log("All ENV Variables:", process.env);
+
+// process.env.NODE_ENV === "production" 
+//   ? PRODUCTION_DOMAIN 
+//   : DEVELOPMENT_DOMAIN;
+
+
+
+//     console.log("Current Environment:", process.env.NODE_ENV);
+// console.log("All ENV Variables:", process.env);
 
