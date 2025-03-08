29233180462a9e2cdede9a3953b672d408774fa4
somaya-77 <alkhtybsmytadl@gmail.com>
2025-03-08 02:38:14 +0200

    solve server build

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -46,7 +46,7 @@ export function setCookie(jwtPayload: JWTPayload): string {
         secure: isProduction, 
         sameSite: isProduction ? "none" : "lax", 
         path: "/",
-        domain: isProduction ? ".snapgram-social-media-app.netlify.app" : "localhost",
+        domain: isProduction ? "https://snap-gram-nu.vercel.app" : "localhost",
         maxAge: 60 * 60 * 24 * 30, 
     });
 
