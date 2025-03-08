d31650aa82f42c8b19bfc17ae78f1ed0b809b2d6
somaya-77 <alkhtybsmytadl@gmail.com>
2025-03-08 14:47:27 +0200

    solve server build

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,6 @@ const nextConfig = {
         ],
     },
     env: {
-        NEXT_PUBLIC_DOMAIN: process.env.NEXT_PUBLIC_DOMAIN,
         CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
         CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
         CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
